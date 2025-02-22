133e0fccf4266fa6a37359a1649ad2ddfe1d0e88
hendriari <106725048+hendriari@users.noreply.github.com>
2025-02-23 01:34:22 +0700

    minor update

--- lib/Others/other_printers_manager.dart
+++ lib/Others/other_printers_manager.dart
@@ -158,7 +158,8 @@ class OtherPrinterManager {
         return;
       } catch (e) {
         log('Failed to print data to device: $e');
-        throw Exception('Failed to print data to device\n$e');
+        String errorMessage = e.toString().replaceAll("Exception: ", "");
+        throw Exception(errorMessage);
       }
     }
   }
